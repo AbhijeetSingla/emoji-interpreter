391a2a96e4349a64ecc959dcbfecfc217f14186e
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-05-19 03:49:18 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Header, App, Footer } from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,14 +12,3 @@ root.render(
     <Footer />
   </React.StrictMode>
 );
-
-// pageBody.render(
-//   <React.StrictMode>
-//     <bodyApp />
-//   </React.StrictMode>
-// )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
